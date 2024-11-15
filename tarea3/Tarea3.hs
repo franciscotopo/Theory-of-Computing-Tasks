d2096453e5c4a1ca8924f9ba8e5eb982ffaae62a
Francisco Topolansky <franciscotopolansky@gmail.com>
2024-11-15 00:03:17 -0300

    Tarea 3

--- tarea3/Tarea3.hs
+++ tarea3/Tarea3.hs
@@ -3,7 +3,12 @@
 
 module MT where 
 
-    import Prelude hiding (iterate)
+    ------------------- Tarea 3 ------------------------
+    -- Nombre: Francisco Topolansky
+    -- Número: 288469
+    ----------------------------------------------------
+    
+    import Prelude hiding (iterate, reverse)
     
 
     -- 1. Definir tipos apropiados para representar los simbolos Σ, estados , cintas, acciones y el código.
@@ -13,7 +18,7 @@ module MT where
     blanck :: Σ 
     blanck = "#"
 
-    type Q = String                 --State o estados
+    type Q = String                 
 
     i :: Q                  
     i = "i"
@@ -25,13 +30,13 @@ module MT where
 
     data Action = L | R | W Σ  
  
-    type M = [(Q, [Branch])]           -- Dado un estado me duevuelve su branch. Es CODE o MAQUINA DE TURING
+    type M = [(Q, [Branch])]           
 
     type Branch = (Σ, (Action, Q))
 
     -- 2. Definir la función (parcial) de ejecución de un código sobre una cinta dada
 
-    exec :: Tape -> M -> Tape       -- ver la clase pasada lo que hicimos con imp (esta funcion usa una auxiliar iteracion que da un paso )
+    exec :: Tape -> M -> Tape       
     exec t m = case (iterate t m i) of {
                 (t', h) -> t'
             }
@@ -39,8 +44,8 @@ module MT where
     type Config = (Tape, Q)     
         
     iterate :: Tape -> M -> Q -> Config
-    iterate t m "h" = (t, h)                                        -- Caso itero desde halt
-    iterate t m q   = case (step t (buscoEstado q m)) of {          -- Caso cinta con estado no halt
+    iterate t m "h" = (t, h)                                        
+    iterate t m q   = case (step t (buscoEstado q m)) of {          
                         (t'', q'') -> iterate (t'') m q''
                     };
 
@@ -63,14 +68,14 @@ module MT where
                         Just algo -> algo; 
                         Nothing -> case (lookup "_" bs) of {
                             Just alg -> alg;
-                            Nothing -> error "No se definieron acciones para esa accion"
+                            Nothing -> error "No se definieron acciones para este simbolo"
                         }                         
                     }
 
 
-    -- Funciones
+    -- 3. Codificar MT embebidas en Haskell que computen los programas:
 
-    left_sigma :: Σ -> M            -- este es el codigo. Luego hago exec con este codigo, que busca el sigma dado 
+    left_sigma :: Σ -> M            
     left_sigma o = [(i,     [("_", (L, "q0"))]),         
                     ("q0",  [(o,      (W o, h )),
                              ("_",    (L, "q0"))
@@ -85,11 +90,37 @@ module MT where
     pr11 = exec t1 (left_sigma "o") == (["#"],"o",["g","g","g","#","#","#","#"])
     pr12 = exec t2 (left_sigma "o") == (["#", "o", "o"],"o",["$","#","#","#","#"])
     pr13 = exec t3 (left_sigma "o") == (["#"],"o",["f","f","f","f","#", "#", "#", "#"])
-
-    -- Elijo tres chirimbolos que quiera y mi maquina funciona para esos tres simbolos. cuando llamo la funcino le paso uno de esos tres
+    all1 = and [pr11, pr12, pr13]
 
     par :: M 
-    par = undefined 
+    par = [(i,      [("_", (L, "q0"))]),
+           ("q0",   [("I",  (L, "q4")),
+                     ("#",  (R, "q1"))
+                    ]),
+           ("q1",   [("#",  (R, "q2")),
+                     ("I",  (R, "q1"))
+                    ]),
+           ("q2",   [("#",  (W "True", "q3"))]),
+           ("q3",   [("True", (R, h))]),
+           ("q4",   [("I",  (L, "q0")),
+                     ("#",  (R, "q5"))
+                    ]),
+           ("q5",   [("I",  (R, "q6"))]),
+           ("q6",   [("I",  (R, "q6")),
+                     ("#",  (R, "q7"))
+                    ]),
+           ("q7",   [("#",  (W "False", "q8"))]),
+           ("q8",   [("False", (R, h))])
+          ] 
+
+    t5 = (["#", "#", "I", "I", "I", "I"],"#",["#", "#"])
+    t6 = (["#", "I"],"#",["#", "#"])
+    t7 = (["#", "#"],"#",["#", "#"])
+
+    pr21 = exec t5 par == (["#","#","I","I","I","I","#","True"],"#",[])
+    pr22 = exec t6 par == (["#","I", "#","False"],"#",[])
+    pr23 = exec t7 par == (["#","#","#","True"],"#",[])
+    all2 = and [pr21, pr22, pr23]
 
     elem_sigma :: Σ -> M
     elem_sigma o = [(i,     [("_", (L, "q0"))]),
@@ -100,86 +131,78 @@ module MT where
                     ("q1",  [("#", (R, "q2")),
                              ("_", (R, "q1"))
                             ]),
-                    ("q2",  [("_", (W "True", h))]),
+                    ("q2",  [("_", (W "True", "q5"))]),
                     ("q3",  [("#", (R, "q4")),
                              ("_", (R, "q3"))
                             ]),
-                    ("q4",  [("_", (W "False", h))])
+                    ("q4",  [("_", (W "False", "q6"))]),
+                    ("q5",  [("True", (R, h))]),
+                    ("q6",  [("False", (R, h))])
                    ] 
     
-    t4 = (["#", "#", "#", "#"], "#" ,["#", "#"])
+    t4 = (["#", "o", "%", "&", "f", "f"],"#",["#", "#", "#"])
 
-    pr31 = exec t1 (elem_sigma "o") == (["#", "o", "g", "g", "g","#"], "True",["#", "#"])
-    pr32 = exec t2 (elem_sigma "o") == (["#", "o", "o", "o", "$", "#"], "True" ,["#", "#"])
-    pr33 = exec t3 (elem_sigma "o") == (["#", "o", "f", "f", "f", "f", "#"], "True",["#", "#"])
-    pr34 = exec t4 (elem_sigma "o") == (["#", "#", "#", "#", "#"], "False" ,["#"])
-    
+    pr31 = exec t1 (elem_sigma "o") == (["#", "o", "g", "g", "g","#", "True"], "#", ["#"])
+    pr32 = exec t2 (elem_sigma "o") == (["#", "o", "o", "o", "$", "#", "True"], "#", ["#"])
+    pr33 = exec t3 (elem_sigma "o") == (["#", "o", "f", "f", "f", "f", "#", "True"], "#", ["#"])
+    pr34 = exec t3 (elem_sigma "$") == (["#", "o", "f", "f", "f", "f", "#", "False"], "#", ["#"])
+    pr35 = exec t3 (elem_sigma "*") == (["#", "o", "f", "f", "f", "f", "#", "False"], "#", ["#"])
+    pr36 = exec t4 (elem_sigma "o") == (["#", "o", "%", "&", "f", "f", "#", "True"], "#", ["#"])
+    pr37 = exec t4 (elem_sigma "%") == (["#", "o", "%", "&", "f", "f", "#", "True"], "#", ["#"])
+    pr38 = exec t4 (elem_sigma "&") == (["#", "o", "%", "&", "f", "f", "#", "True"], "#", ["#"])
+    pr39 = exec t4 (elem_sigma "f") == (["#", "o", "%", "&", "f", "f", "#", "True"], "#", ["#"])
+    pr310 = exec t4 (elem_sigma ")") == (["#", "o", "%", "&", "f", "f", "#", "False"], "#", ["#"])
+    pr311 = exec t4 (elem_sigma "@") == (["#", "o", "%", "&", "f", "f", "#", "False"], "#", ["#"])
+    all3 = and [pr31, pr32, pr33, pr34, pr35, pr36, pr37, pr38, pr39, pr310, pr311]
 
     reverse :: M 
-    reverse = undefined
+    reverse = [(i,      [("#", (L,     "q0"))]),
+               ("q0",   [("a", (W "X", "q3")),
+                         ("b", (W "Y", "q9")),
+                         ("#", (R,     "q1"))
+                        ]),
+               ("q3",   [("X", (R,     "q4"))]),  
+               ("q4",   [("#", (R,     "q5")),
+                         ("_", (R,     "q4"))
+                        ]),
+               ("q5",   [("#", (W "a", "q6")),
+                         ("_", (R,     "q5"))
+                        ]),
+               ("q6",   [("a", (L,     "q7"))]),
+               ("q7",   [("X", (W "a", "q8")),
+                         ("_", (L,     "q7"))
+                        ]),
+               ("q8",   [("a", (L,     "q0"))]),
+               ("q9",   [("Y", (R,     "q10"))]),
+               ("q10",  [("#", (R,     "q11")),
+                         ("_", (R,     "q10"))
+                        ]),
+               ("q11",  [("#", (W "b", "q12")),
+                         ("_", (R,     "q11"))
+                        ]),
+               ("q12",  [("b", (L,     "q13"))]),
+               ("q13",  [("Y", (W "b", "q14")),
+                         ("_", (L,     "q13"))
+                        ]),
+               ("q14",  [("b", (L,     "q0"))]),
+               ("q1",   [("#", (R,     "q2")),
+                         ("a", (R,     "q1")),
+                         ("b", (R,     "q1"))
+                        ]),
+               ("q2",   [("#", (W "#",  h)),
+                         ("a", (R,     "q2")),
+                         ("b", (R,     "q2"))
+                        ])                          
+              ]
 
+    t8 = (["#", "#", "b", "b", "b", "a", "a", "a"], "#", ["#", "#", "#", "#", "#", "#", "#", "#"])
+    t9 = (["#", "b", "a", "b", "a", "b", "a"], "#", ["#", "#", "#", "#", "#", "#", "#", "#"])
+    t10 = (["#", "b", "a"], "#", ["#", "#", "#", "#", "#"])
 
+    pr41 = exec t8 reverse  == (["#", "#", "b", "b", "b", "a", "a", "a", "#", "a", "a", "a", "b", "b", "b"], "#" ,["#"])
+    pr42 = exec t9 reverse  == (["#", "b", "a", "b", "a", "b", "a", "#", "a", "b", "a", "b", "a", "b"], "#" ,["#"])
+    pr43 = exec t10 reverse == (["#", "b", "a", "#", "a", "b"], "#" ,["#", "#"])
+    all4 = and [pr41, pr42, pr43]
 
+    allTests = and [all1, all2, all3, all4]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- -- Simbolos: Uno de los simbolos es # denominado blanco.
--- Sigma alfabeto = Tiene al menos dos simbolos. Blank # y comodin _
--- -- Nuestro grafo es una LA. 
-
-
-
-
--- --La ejecucion o control (PROGRAMA EN MT) 
-
--- La clave es un estado inicial.
-
--- la clave es i y tiene otra tabla.
--- Desde i puedo ir a varios lugares
-
--- estado inicial | simbolo || a   |    nuevo estado
--- i                   #       r           q0
--- i                   a       l           q1 
--- i                   b       #           i 
-
--- Para no tener que escribir varias veces i, tengo otra tabla que me representa los estados de i 
-
--- Ahora tengo esto. Una tabla con estados iniciales que van a otra tabla de ramas con las branches del estado
-
--- (estado inicial ) | simbolo || a   |    nuevo estado
--- i  ------------->   #       r           q0
---                     a       l           q1 
---                     b       #           i 
-
--- Tengo una entrada para el estado i y me determina una tabla. Y dependiendo el simbolo, me determina un aaccion y un nuevo estado. 
-
--- La tabla de ramas tiene como clave el simbolo del alfabeto o un comodin _, y el dato asociado a la clave es una pareja accion estado 
-
--- -- Acciones
-
--- l, r, sigma o, donde o es cualquier simbolo con los soguientes significados
--- left mueve a la izquierda
--- right a la derecha
--- sigma sobreescribe el simbolo corriente por sigma y deja el cabezal sobre el simbolo escrito
-
--- -- TABLA
-
--- PARA CADA ESTADO TENGO UNA TABLITA CON SUS BRANCHES = (simbolo leido, (accion y estado))
--- Lkista de parejas i - tabla. Y la tabla es una lista de parejas simbolo - pareja (a, q')
-
--- [(i, [(o, [(a, q')])])] 
-
--- Hay dos elementos distinguidos. i de init y h de halt. Luego hay finitos otros elementos.
--- El estado terminal no aparece como clave en ninguna entrada d ela tabla
